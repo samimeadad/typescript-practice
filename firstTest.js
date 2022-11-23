34cc06a99f35197ba90a77f711fe8e60e4b7b61b
samimeadad <sami.meadad@gmail.com>
2022-11-23 10:04:38 +0600

    first commit

---
+++ firstTest.js
@@ -0,0 +1,6 @@
+var myName = "My name is Sami";
+console.log(myName);
+var x = 10;
+var y = 12;
+var sum = x + y;
+console.log(sum);
---
+++ firstTest.ts
@@ -0,0 +1,7 @@
+let myName: string = "My name is Sami";
+console.log( myName );
+
+let x: number = 10;
+let y: number = 12;
+let sum: number = x + y;
+console.log( sum );
